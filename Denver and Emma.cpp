2db883b04e9312183fa55ee230d760be3ac1f916
J3T <64909254+Ab0rter@users.noreply.github.com>
2021-11-30 15:32:16 +0100

    Create Denver and Emma.cpp
    SPOJ Denver and Emma cpp solution

---
+++ Denver and Emma.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	int t;
+	float n,a,b,r;
+	cin >> t;
+	
+	while(t--)
+	{
+		cin >> n >> a >> b;
+		r = (n*a)+b;
+		cout << r << "\n";
+	}
+	return 0;
+}  
